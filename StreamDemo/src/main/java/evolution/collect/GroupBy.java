d4e5e9aeb136da33c13b969f5c0237c7da5b5308
chenli <chenli@192.168.43.24>
2017-11-06 16:52:37 +0800

    new demos

--- StreamDemo/src/main/java/evolution/collect/GroupBy.java
+++ StreamDemo/src/main/java/evolution/collect/GroupBy.java
@@ -14,4 +14,11 @@ public class GroupBy {
 		.stream().collect(Collectors.groupingBy(AnyEntity::getName));
 		System.out.println(map);
 	}
+	
+	@Test
+	public void test0() {
+		Map<String, List<AnyEntity>> map = Arrays.asList(new AnyEntity("Chen", "M"), new AnyEntity("Chen", "F"), new AnyEntity("Ling", "M"), new AnyEntity("Ling", "F"))
+		.stream().collect(Collectors.groupingBy(x -> x.getName()));
+		System.out.println(map);
+	}
 }
